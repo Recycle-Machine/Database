83763d3595583127a2f33de2eb32004baba67bed
LuisSigala <a6519150031@utch.edu.mx>
2021-07-08 20:50:05 -0600

    Database Connection

---
+++ db_config.py
@@ -0,0 +1,13 @@
+""" FLask configuration to connect to the database """
+from flask_pymongo import pymongo
+import os
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+
+
+client = pymongo.MongoClient(f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@db-example.du4lj.mongodb.net/{DB_NAME}?retryWrites=true&w=majority")
+
+# Down here you need to add the database name
+db = client.Integrative_db
