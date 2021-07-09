047de9e8ff0802003265fc41624b5e70702428ef
LuisSigala <a6519150031@utch.edu.mx>
2021-07-09 02:51:46 -0600

    Links changes

--- main.py
+++ main.py
@@ -4,12 +4,17 @@ from flask_pymongo import pymongo
 import db_config as database
 
 #Resources
-from badge import User
+from res.user import User
+from res.profile import Profile
+from res.transactions import Transactions
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(User, '/new/', '/<string:by>:<string:data>/')
+api.add_resource(Transactions, '/new/transactions/<string:_id>/', '/transactions/<string:_id>/')
+api.add_resource(Profile, '/new/profile/<string:_id>/', '/profile/<string:_id>/', '/<string:_id>/<string:uuid>/')
+
 
 if __name__ == '__main__':
     app.run(load_dotenv = True)
