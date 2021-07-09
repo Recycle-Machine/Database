42967ec53957dd96104cceac42db000e5d7d4b21
LuisSigala <a6519150031@utch.edu.mx>
2021-07-09 02:34:42 -0600

    Abort if user not exist function

--- res/transactions.py
+++ res/transactions.py
@@ -13,3 +13,11 @@ class Transactions(Resource):
         return jsonify(response)
 
     
+
+    def abort_if_not_exist(self, _id):
+        response = database.db.Badges.find_one({'_id':ObjectId(_id)}, {"name": 1, "transactions": 1})
+
+        if response:
+            return response
+        else:
+            abort(jsonify({"status": 404, "_id": f"{_id} not found"}))
