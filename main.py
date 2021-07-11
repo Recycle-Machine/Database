0ffe4a651db25d0284fb96c3a535415a8e7430f3
LuisSigala <a6519150031@utch.edu.mx>
2021-07-10 18:51:32 -0600

    links changes

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ import db_config as database
 #Resources
 from res.user import User
 from res.profile import Profile
-from res.transactions import Transactions
+from res.goal import Goal
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(User, '/new/', '/<string:by>:<string:data>/')
-api.add_resource(Transactions, '/new/transactions/<string:_id>/', '/transactions/<string:_id>/')
 api.add_resource(Profile, '/new/profile/<string:_id>/', '/profile/<string:_id>/', '/<string:_id>/<string:uuid>/')
+api.add_resource(Goal, '/new/goal/<string:_id>/', '/goal/<string:_id>/')
 
 
 if __name__ == '__main__':
